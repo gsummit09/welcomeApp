0304106f8b080c5cce716def3f1a280ceb9a828c
gsummit09 <nayaksumit998@gmail.com>
2024-02-15 12:05:39 +0530

    Final Commit

---
+++ SignIn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class SignIn
+    {
+        public string Authentication(string username, string password)
+        {
+            string msg;
+            if(string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                msg = "User name and psaaword required";
+            }
+            else
+            {
+                if((username == "admin" ) && (password == null))
+                {
+                    msg = "Authentication Pass";
+                }  
+                else
+                {
+                    msg = "Authentication Fail";
+                }
+            }
+            return msg;
+        }
+    }
+}
